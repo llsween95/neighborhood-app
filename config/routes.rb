32f9a9435eeca7a56debd21340efba3d670321b1
Luke Sweeney <llsween95@gmail.com>
2020-06-10 15:27:12 -0400

    working on routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,13 @@ Rails.application.routes.draw do
   end
   post '/auth/login', to: 'authentication#login'
   get '/auth/verify', to: 'authentication#verify'
+
+  get '/posts', to: 'posts#index'
+  get '/posts/:posts_id', to: 'posts/#show'
+
+  get '/posts/:posts_id/comments', to: 'comments#index'
+  get '/posts/:posts_id/comments/:comments_id', to: 'comments#show'
+
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,7 @@ User.destroy_all
 @user1 = User.create!(name: "Sweeney", email: "sweendog@email.com", password:"123456")
 @post1 = Post.create!(content: "Hey, it's Sweendog here.", user:@user1 )
 @comment1 = Comment.create!(content:"I am commenting on my own post.", user:@user1, post:@post1)
+
+@user2 = User.create!(name: "John Johnson", email: "jj@email.com", password:"123456")
+@post2 = Post.create!(content: "John Johnson, reporting for duty.", user:@user2 )
+@comment2 = Comment.create!(content:"John Johnson here commenting on my own post.", user:@user2, post:@post2)
