744d1192a4d80d7c53a20792bc7d6d818133049e
Luke Sweeney <llsween95@gmail.com>
2020-06-13 20:42:00 -0400

    update post complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,7 +29,7 @@ class PostsController < ApplicationController
   # PATCH/PUT /posts/1
   def update
     if @post.update(post_params)
-      render json: @post
+      render json: @post, include:[{ user:{only:[:id, :name]} }, {comments: {include: {user:{only: [:id, :name]}}}}]
     else
       render json: @post.errors, status: :unprocessable_entity
     end
